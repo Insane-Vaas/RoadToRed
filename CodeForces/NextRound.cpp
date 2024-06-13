7bbee14ff21ba762e0186036884912190c629127
Insane-Vaas <hrithikr200@gmail.com>
2024-06-13 22:51:19 +0530

    Codeforces Practice

--- CodeForces/NextRound.cpp
+++ CodeForces/NextRound.cpp
@@ -1,18 +1,23 @@
 #include<iostream>
+#include <vector>
 using namespace std;
 #define ll long long
 
 
 int main(){
 
-    ll n = 0;
-    cin>>n;
+    ll n = 0, k = 0;
+    cin>>n>>k;
     ll res = 0;
-    while(n--){
+    vector<ll> v(n);
+    for(int i=0;i<n;i++){
+        cin>>v[i];
+    }
 
+    for(int i=0;i<n;i++){
+        if(v[k-1] <= v[i] && v[i] > 0)res++;
     }
 
     cout<<res<<endl;
-
     return 0;
 }
