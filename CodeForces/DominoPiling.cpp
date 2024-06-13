12a2081d3e8dd04f85e772110c67273510c65349
Insane-Vaas <hrithikr200@gmail.com>
2024-06-13 23:49:50 +0530

    Codeforces problem solving

---
+++ CodeForces/DominoPiling.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include <vector>
+using namespace std;
+#define ll long long
+
+
+int main(){
+
+    ll n = 0, m = 0;
+    cin>>n>>m;
+
+    ll res = (n*m)/2;
+
+    cout<<res<<endl;
+    return 0;
+}
